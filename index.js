e072e54339dbf80bb9bfb30a2b34d0a206c33a80
prsanthib <prasanthibammidi10@gmail.com>
2024-05-22 22:25:50 +0530

    validated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 
+
 connectToMongo();
 const app = express()
 const port = 3000
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router(); 
-
+const { body, validationResult } = require('express-validator');
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res)=>{ 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
-} )
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password').isLength({ min: 5 })
+], async (req, res) => { 
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({ errors: result.array() });
+    }
 
-module.exports = router
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password  
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
