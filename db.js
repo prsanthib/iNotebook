295ae9ca9a70cf77287aa1713cc5ee7c9ff9c3aa
prsanthib <prasanthibammidi10@gmail.com>
2024-05-22 20:00:24 +0530

    added data to mongodb

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/"
 const connectToMongo = async()=>{
-// mongoose.connect(mongoURI,()=>{
-//     console.log("connected to mongoose");
-// })
 mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
 }
 module.exports = connectToMongo;
